991b03e2788d64dcb6bc544f124ea0135d3eafd2
boneyk <124228691+boneyk@users.noreply.github.com>
2025-12-10 01:16:22 +1000

    Some fetch tasks was solved and new file for async tasks was added

---
+++ fetch.js
@@ -0,0 +1,9 @@
+// fetch("https://jsonplaceholder.typicode.com/users/1").then(res => res.json()).then(data=>console.log(data.phone)).catch(console.error);
+
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((res) => res.json())
+//   .then((data) => {
+//       let a = data.slice(0, 5).map((i) => i.title);
+//       console.log(a)
+//   })
+//   .catch(console.error);
